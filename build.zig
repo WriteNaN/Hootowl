3705953a875f5569d88a6a5be371dcc803dbbc4d
Write Int <151211283+WriteNaN@users.noreply.github.com>
2024-03-18 00:04:06 +0530

    push changes (zig v12)

--- build.zig
+++ build.zig
@@ -2,15 +2,53 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-
     const optimize = b.standardOptimizeOption(.{});
 
+    // w
+    _ = b.addModule("webview", .{ .root_source_file = .{ .path = "src/webview/webview.zig" }, .optimize = optimize, .target = target });
+
+    const sharedLib = b.addSharedLibrary(.{
+        .name = "webviewShared",
+        .optimize = optimize,
+        .target = target,
+    });
+    sharedLib.defineCMacro("WEBVIEW_BUILD_SHARED", null);
+    sharedLib.linkLibCpp();
+
+    switch (target.query.os_tag orelse @import("builtin").os.tag) {
+        .windows => {
+            sharedLib.addCSourceFile(.{ .file = .{ .path = "src/webview/lib/webview/webview.cc" }, .flags = &.{"-std=c++14"} });
+            sharedLib.addIncludePath(std.Build.LazyPath.relative("src/webview/lib/WebView2/"));
+            sharedLib.linkSystemLibrary("ole32");
+            sharedLib.linkSystemLibrary("shlwapi");
+            sharedLib.linkSystemLibrary("version");
+            sharedLib.linkSystemLibrary("advapi32");
+            sharedLib.linkSystemLibrary("shell32");
+            sharedLib.linkSystemLibrary("user32");
+        },
+        .macos => {
+            sharedLib.addCSourceFile(.{ .file = .{ .path = "src/webview/lib/webview/webview.cc" }, .flags = &.{"-std=c++11"} });
+            sharedLib.linkFramework("WebKit");
+        },
+        else => {
+            sharedLib.addCSourceFile(.{ .file = .{ .path = "src/webview/lib/webview/webview.cc" }, .flags = &.{"-std=c++11"} });
+            sharedLib.linkSystemLibrary("gtk+-3.0");
+            sharedLib.linkSystemLibrary("webkit2gtk-4.0");
+        },
+    }
+    b.installArtifact(sharedLib);
+
+    // end w
+
     const exe = b.addExecutable(.{
-        .name = "Hootowl",
+        .name = "hootowl",
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
+
+    exe.linkLibC();
+    exe.linkLibrary(sharedLib); 
 
     b.installArtifact(exe);
 
@@ -22,18 +60,6 @@ pub fn build(b: *std.Build) void {
         run_cmd.addArgs(args);
     }
 
-    const run_step = b.step("run", "Run the app");
+    const run_step = b.step("run", "Run the program");
     run_step.dependOn(&run_cmd.step);
-
-
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,11 @@
 const std = @import("std");
+const WebView = @import("./webview/webview.zig").WebView;
 
 pub fn main() !void {
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const w = WebView.create(false, null);
+    defer w.destroy();
+    w.setTitle("Basic Example");
+    w.setSize(480, 320, WebView.WindowSizeHint.None);
+    w.setHtml("Thanks for using webview!");
+    w.run();
 }
